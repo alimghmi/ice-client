6d32cf1b505e46f288741849d1c76d97f4b69d6c
Ali Moghimi <mghmiali@gmail.com>
2023-10-21 21:42:27 +0000

    feat: add logging to main module

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from utils import create_inserter_objects
 
 
 def main():
-    logger.info("Initializing Application")
+    logger.info("Initializing Scraper Engine")
     engine = Engine()
     df = engine.fetch()
+    logger.info("Transforming Data")
     df_transformed = Agent(df).transform()
+    logger.info("Preparing Database Inserter")
     inserter = create_inserter_objects()
-    result = inserter.insert(df_transformed, settings.OUTPUT_TABLE)
-    print(result)
+    logger.info(f"Inserting Data into {settings.OUTPUT_TABLE}")
+    inserter.insert(df_transformed, settings.OUTPUT_TABLE)
+    logger.info("Application completed successfully")
     return
 
 
