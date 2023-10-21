d35738a28568687216d426076e7e5908ca16b83c
Ali Moghimi <mghmiali@gmail.com>
2023-10-21 21:31:13 +0000

    feat: add config module

--- config/logger.py
+++ config/logger.py
@@ -4,7 +4,7 @@ from config.settings import LOG_LEVEL
 
 logging.basicConfig(
     level=getattr(logging, LOG_LEVEL),
-    format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] [%(funcName)s()] - %(message)s",
+    format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] [%(funcName)s()] - %(message)s",  # noqa: E501
 )
 
 logger = logging.getLogger()
--- config/settings.py
+++ config/settings.py
@@ -2,3 +2,9 @@ from decouple import config
 
 URL = config("URL")
 LOG_LEVEL = config("LOG_LEVEL", default="INFO")
+OUTPUT_TABLE = config("OUTPUT_TABLE")
+INSERTER_MAX_RETRIES = config("INSERTER_MAX_RETRIES", default=3, cast=int)
+MSSQL_SERVER = config("MSSQL_SERVER")
+MSSQL_DATABASE = config("MSSQL_DATABASE")
+MSSQL_USERNAME = config("MSSQL_USERNAME")
+MSSQL_PASSWORD = config("MSSQL_PASSWORD")
