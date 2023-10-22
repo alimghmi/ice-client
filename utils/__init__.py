76c8432a83596f74f6abf43772e9b90705b357f8
Ali Moghimi <mghmiali@gmail.com>
2023-10-22 21:17:21 +0000

    feat: add request helper class

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from .db_helper import create_inserter_objects
+from .request_helper import Request
---
+++ utils/request_helper.py
@@ -0,0 +1,24 @@
+import time
+
+import requests
+
+from config import logger
+
+
+class Request:
+    def __init__(self, max_retries, backoff_factor) -> None:
+        self.max_retries = max_retries
+        self.backoff_factor = backoff_factor
+
+    def request(self, method: str, url: str, *args, **kwargs) -> requests.Request:
+        for i in range(self.max_retries):
+            try:
+                return requests.request(method=method, url=url, *args, **kwargs)
+            except requests.exceptions.RequestException as e:
+                logger.error(
+                    f"{method.upper()} request failed. Attempt {i + 1} of {self.max_retries}. Error: {e}"  # noqa: E501
+                )
+                if (i + 1) == self.max_retries:
+                    raise
+
+                time.sleep(i**self.backoff_factor)
