058f51150c96f5cb902fecdd4a9e45a563f99642
Ali Moghimi <mghmiali@gmail.com>
2023-10-23 10:12:07 +0000

    feat: add column brakedown function

--- transformer/agent.py
+++ transformer/agent.py
@@ -10,7 +10,13 @@ class Agent:
         "Clearing Date": "clearing_date",
         "Name": "name",
         "Instrument Name": "instrument_name",
-        "EOD Price": "price",
+        "Ticker": "ticker",
+        "Tier": "tier",
+        "Currency": "currency",
+        "Doc Clause": "doc_clause",
+        "Fixed Rate": "fixed_rate",
+        "Maturity Date": "maturity_date",
+        "EOD Price": "price"
     }
 
     def __init__(self, df: pd.DataFrame) -> None:
@@ -19,6 +25,8 @@ class Agent:
     def transform(self) -> pd.DataFrame:
         logger.info("Starting data transformation.")
         try:
+            self.brakedown_instrument_name()
+            logger.debug("Broke down instrument successfully.")
             self.parse_date()
             logger.debug("Parsed dates successfully.")
             self.add_timestamp()
@@ -38,6 +46,9 @@ class Agent:
             self.dataframe["Clearing Date"] = pd.to_datetime(
                 self.dataframe["Clearing Date"], format="%Y-%m-%d"
             )
+            self.dataframe["Maturity Date"] = pd.to_datetime(
+                self.dataframe["Maturity Date"], format="%Y-%m-%d"
+            )
         except Exception as e:
             logger.error(f"Failed to parse dates. Error: {e}")
             raise
@@ -54,4 +65,14 @@ class Agent:
             self.dataframe.rename(columns=self.COLUMNS_NAME, inplace=True)
         except Exception as e:
             logger.error(f"Failed to rename columns. Error: {e}")
+            raise
+        
+    def brakedown_instrument_name(self):
+        try:
+            df_split = self.dataframe['Instrument Name'].str.split('.', n=5, expand=True)
+            print(df_split)
+            df_split.columns = ['Ticker', 'Tier', 'Currency', 'Doc Clause', 'Fixed Rate', 'Maturity Date']
+            self.dataframe = pd.concat([self.dataframe, df_split], axis=1)
+        except Exception as e:
+            logger.error(f"Failed to brake down instrument name. Error: {e}")
             raise
